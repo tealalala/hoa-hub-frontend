9f2458ddf76ccf590baade8bebd5ab86b0d1882d
Tea <tdinhla@protonmail.com>
2018-11-25 12:31:10 -0800

    add login to views, router.js and main.js

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
 import Signup from "./views/Signup.vue";
+import Login from "./views/Login.vue";
+
 
 Vue.use(Router)
 
@@ -24,6 +26,11 @@ export default new Router({
       path: "/signup",
       name: "signup",
       component: Signup
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
     }
   ]
 })
