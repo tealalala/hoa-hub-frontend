8ae5bdab6161634209704f4b3ee75e3ede02ba63
Tea <tdinhla@protonmail.com>
2018-12-10 19:28:34 -0800

    change data display to a table in violations-index.vue

--- src/views/Violations-Index.vue
+++ src/views/Violations-Index.vue
@@ -4,17 +4,47 @@
       <h1>Violations Index</h1>
 
       <!-- <div v-for="action in filterBy(action_by_address_users, true, 'is_violation')"> -->
-      <div v-for="action in filtered_violations_is_true">
+      <!-- <div v-for="action in filtered_violations_is_true">
         <p>violation category: {{ action.category }} test</p>
         <p>status: {{ action.status }} test</p>
         <hr>
-      </div>
+      </div> -->
+
+      <table width="100%" border=1 cellpadding=10 cellspacing=10>
+        <tr bgcolor=#eee>
+          <th width="30%">Violation Category</th>
+          <th width="50%">Description</th>
+          <th width="10%">Status</th>
+          <th width="10%">Resolved?</th>
+          <th width="10%">Action</th>
+        </tr>
+        <tr v-for="action in filtered_violations_is_true">
+          <td>{{ action.category }}</td>
+          <td>{{ action.description }}</td>
+          <td>{{ action.is_violation }}</td>
+          <td>{{ action.status }}</td>
+          <td><a href="#" class="btn btn-danger">Resolve this Violation</a></td>
+        </tr>
+      </table>
+
+
+      <!-- <div class="w-auto p-3" v-for="action in filtered_violations_is_true">
+        <div class="card-body">
+          <h5 class="card-title">{{ action.category }} Violation</h5>
+          <p class="card-text">Description: {{ action.description }}</p>
+          <a href="#" class="btn btn-danger">Resolve this Violation</a>
+          <hr>
+        </div>
+      </div> -->
 
     </div>
   </div>
 </template>
 
 <style>
+table {
+  margin-top: 30px;
+}
 </style>
 
 <script>
