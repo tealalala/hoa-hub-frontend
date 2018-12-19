383a7ac390fce1b3d1bf688ceaf92e9cf091d376
Tea <tdinhla@protonmail.com>
2018-12-18 19:26:42 -0800

    add new ccrs data

--- src/views/Ccrs.vue
+++ src/views/Ccrs.vue
@@ -13,18 +13,17 @@
         <hr>
       </div> -->
 
+      <p>THIS DECLARATION OF COVENANTS, CONDITIONS AND RESTRICTIONS OF TOWNSEND MAPLE ("Declaration") is made by TOWNSEND HOMES, INC., a California corporation, ("Declarant").</p>
+
       <table width="100%" border=1 cellpadding=10 cellspacing=10>
         <thead bgcolor=#eee>
           <th width="5%" scope="col" style="text-align: left; width: 10rem;">
-            ID
+            Article <br/>& Section
           </th>
           <th width="5%" scope="col" style="text-align: left; width: 10rem;">
-            Status
+            Header
           </th>
-          <th width="5%" scope="col" style="text-align: left; width: 10rem;">
-            Section
-          </th>
-          <th width="55%" scope="col" style="text-align: left; width: 10rem;">
+          <th width="80%" scope="col" style="text-align: left; width: 10rem;">
             Description
           </th>
           <th width="10%" scope="col" style="text-align: left; width: 10rem;">
@@ -33,9 +32,9 @@
         </thead>
         <tbody>
           <tr v-for="action in filtered_ccrs_is_true">
-            <td>{{ action.id }}</td>
-            <td>{{ action.status }}</td>
-            <td>{{ action.section }}</td>
+            <!-- <td>{{ action.id }}</td> -->
+            <td>{{ action.article }}.{{ action.section }}</td>
+            <td>{{ action.header }}</td>
             <td>{{ action.description }}</td>
             <td>{{ action.date_approved }}</td>
           </tr>
